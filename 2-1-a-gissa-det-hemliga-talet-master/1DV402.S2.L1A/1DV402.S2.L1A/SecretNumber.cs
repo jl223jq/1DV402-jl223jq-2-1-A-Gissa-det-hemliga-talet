ed3c7ae325d125a68637a6196bc7ab622c7f71a3
Jonas Lingblom <jl223jq@student.lnu.se>
2014-10-06 19:39:05 +0200

    uppg 2.1 måstan klar

---
+++ 2-1-a-gissa-det-hemliga-talet-master/1DV402.S2.L1A/1DV402.S2.L1A/SecretNumber.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1DV402.S2.L1A
+{
+    class SecretNumber
+    {
+        public const int MaxNumberOfGuesses = 7;
+        private int _number;
+        private int _count;
+
+        public void Initialize()
+        { //Skapar 
+            Random randomNumber = new randomNumber();
+            _number = randomNumber.Next(1, 101);
+            _count = 0;
+        }
+
+        public bool MakeGuess(int number)
+        { //Publicering av gissningarna här
+
+            bool guess = false;
+            
+
+            if (number > randomNumber.Next || number < randomNumber.Next)
+            {
+                throw new ArgumentOutOfRangeException("Sa jag inte att du bara fick gissa på ett tal mellan 1 och 100? Gissa igen nu då.");
+            }
+            _count++;
+            if (number == _number)
+            {
+                Console.WriteLine("Rätt! Du använde {0} gissningar.", _count);
+                guess = true;
+            }
+            if (number < _number)
+            {
+                Console.WriteLine("För lågt! Du har {0] gissningar kvar", _count);
+            }
+            if (number > _number)
+            {
+                Console.WriteLine("För högt! Du har {0] gissningar kvar", _count);
+            }
+            
+            
+            if (_count > MaxNumberOfGuesses)
+            {
+                throw new ApplicationException("Du har redan fått dina 7 gissningar.");
+            }
+            return guess;
+        }
+
+
+        public SecretNumber()
+        {//Konstruktor
+            Initialize();
+        }
+    }
+}
